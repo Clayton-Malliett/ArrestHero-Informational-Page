3954862e9e167edfb786979e0335abc15df566a7
Angel Delgado <75641347+clarkdelgado@users.noreply.github.com>
2021-10-08 09:10:50 -0500

    Add files via upload

---
+++ HW3-Angel/index.js
@@ -0,0 +1,34 @@
+// Parallax Function
+let arrestHero = document.getElementById('arrestHero');
+let freedom =document.getElementById('freedom');
+
+window.addEventListener('scroll', () =>{
+    let { scrollY } = window;
+
+    arrestHero.style.top = 0.5 * scrollY + 'px';
+    freedom.style.top = (50 + 0.5 * scrollY) + 'px';
+});
+
+// test... still in progress
+const haeder = document.querySelector("header");
+const sectionOne = document.querySelector(".title");
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+  };
+  
+  const sectionOneObserver = new IntersectionObserver(function(
+    entries, sectionOneObserver) 
+    {
+        entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        header.classList.add("nav-scrolled");
+      } else {
+        header.classList.remove("nav-scrolled");
+      }
+    });
+  },
+  sectionOneOptions);
+  
+  sectionOneObserver.observe(sectionOne);
+//   end of test
